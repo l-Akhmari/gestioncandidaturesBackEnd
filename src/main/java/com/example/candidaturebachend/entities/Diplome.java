5615cb43ebe3be17ebf12ce1fdb9c03a49a33143
NezhaOUBELLA <oubella.nezha2002@gmail.com>
2023-04-29 01:57:37 +0100

    Add class NotesSemester

--- src/main/java/com/example/candidaturebachend/entities/Diplome.java
+++ src/main/java/com/example/candidaturebachend/entities/Diplome.java
@@ -23,22 +23,13 @@ public class Diplome {
     private String specialiteDiplome;
     private Date anneeObtention;
     private String etablissement;
-    private String notesSemester;
+    //private String notesSemester;
     @ManyToOne
     private Candidat candidat;
     @OneToMany(mappedBy = "diplome")
     private List<Filiere> filieres;
     @OneToOne(mappedBy = "diplome")
     private Fichier fichier;
-
-    public void setNotesSemester(List<Double> notes) {
-        this.notesSemester = notes.stream().map(Object::toString)
-                .collect(Collectors.joining(","));
-    }
-
-    public List<Double> getNotesSemester() {
-        return Arrays.stream(notesSemester.split(","))
-                .map(Double::parseDouble)
-                .collect(Collectors.toList());
-    }
+    @OneToMany(mappedBy = "diplome")
+    private List<NotesSemester> notesSemester;
 }

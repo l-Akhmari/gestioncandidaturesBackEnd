154b71397c6e0a130cc86e37b7d7a4bf418331c7
fatima-afa <afafatima12@gmail.com>
2023-06-04 23:27:47 +0200

    add diplome service

---
+++ src/main/java/com/example/candidaturebachend/servicesDto/serviceImpDto/CandidatDtoImp.java
@@ -0,0 +1,19 @@
+package com.example.candidaturebachend.servicesDto.serviceImpDto;
+
+import com.example.candidaturebachend.dto.DiplomeDto;
+import com.example.candidaturebachend.repositories.DiplomeRepository;
+import com.example.candidaturebachend.servicesDto.serviceinterDto.IDiplomeDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+@Slf4j
+@Transactional
+public class CandidatDtoImp {
+
+
+}
